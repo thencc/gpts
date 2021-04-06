6f5034fb0ad58f6f8ecb94817c4b0e853c8d89f8
spencercap <cappiello.spencer@gmail.com>
2021-04-07 00:10:44 +0200

    file reading + injecting

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@
 import { inject } from './utils';
 
 
-let qwe = 'test {0} {1} and then some more'
-qwe = inject(qwe, '123', '456');
-console.log('qwe', qwe);
+// let qwe = 'test {0} {1} and then some more';
+// qwe = inject(qwe, '123', '456');
+// console.log('qwe', qwe);
+
+// needs "@types/node": "^14.14.37",
+import * as fs from 'fs';
+import * as path from 'path';
+
+const f = fs.readFileSync(path.join(__dirname, '../demo/prompts/artist.txt'), 'utf8');
+// console.log('f', f);
+let f2 = inject(f, '123', '456');
+console.log('f2', f2);
