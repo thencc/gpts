04b2e97eb4e8f044c69fa018abbb47cc3555401e
spencercap <cappiello.spencer@gmail.com>
2021-04-07 16:15:32 +0200

    chrono-ts -> gpt-ts

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# chronology-ts
-conversational gpt-x
+# gpt-ts
+gpt-3 / gpt-x typescript wrapper of REST api
 
-typscript version of python lib [chronology](https://github.com/OthersideAI/chronology#fetch_max_search_doc)
 
 ---
 
-works in frontend js/ts or backend node.js / ts-node (backend via node-fetch)
+```ts
+import { GptTs } from 'gpt-ts';
+const brains = new GptTs(OPENAI_APIKEY);
+
+brains.search(...)
+```
+
+---
+
+works in frontend + backend
+
+(backend via node-fetch)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './utils';
+export * from './typings'; // for use elswehere
 import { AnswerRequest, ClassificationRequest, CompletionRequest, EngineId, SearchRequest } from './typings';
 
 // in case this is not the web import fetch
@@ -7,35 +8,37 @@ import fetch from 'node-fetch';
 export class Chronology {
 	hello = 'world';
 	apiKey: string;
+	getHeaders = {
+		Authorization: 'Bearer'
+	};
+	postHeaders = {
+		Authorization: 'Bearer',
+		'Content-Type': 'application/json'
+	}
 
 	constructor(apiKey: string) {
 		// console.log('Chronolgy constructed');
 		this.apiKey = apiKey;
+		this.getHeaders.Authorization = `Bearer ${this.apiKey}`;
+		this.postHeaders.Authorization = `Bearer ${this.apiKey}`;
 	}
 
 	async listEngines(): Promise<any> {
 		return await fetch('https://api.openai.com/v1/engines', {
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`
-			}
+			headers: this.getHeaders
 		});
 	}
 
 	async retrieveEngine(engineId: EngineId): Promise<any> {
 		return await fetch(`https://api.openai.com/v1/engines/${engineId}`, {
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`
-			}
+			headers: this.getHeaders
 		});
 	}
 
 	async createCompletion(engineId: EngineId, options: CompletionRequest): Promise<any> {
 		return await fetch(`https://api.openai.com/v1/engines/${engineId}/completions`, {
 			body: JSON.stringify(options),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
+			headers: this.postHeaders,
 			method: 'POST'
 		});
 	}
@@ -49,10 +52,7 @@ export class Chronology {
 	async createSearch(engineId: EngineId, options: SearchRequest): Promise<any> {
 		return await fetch(`https://api.openai.com/v1/engines/${engineId}/search`, {
 			body: JSON.stringify(options),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
+			headers: this.postHeaders,
 			method: 'POST'
 		});
 	}
@@ -64,10 +64,7 @@ export class Chronology {
 		};
 		return await fetch('https://api.openai.com/v1/classifications', {
 			body: JSON.stringify(bod),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
+			headers: this.postHeaders,
 			method: 'POST'
 		});
 	}
@@ -84,10 +81,7 @@ export class Chronology {
 		};
 		return await fetch('https://api.openai.com/v1/answers', {
 			body: JSON.stringify(bod),
-			headers: {
-				Authorization: `Bearer ${this.apiKey}`,
-				'Content-Type': 'application/json'
-			},
+			headers: this.postHeaders,
 			method: 'POST'
 		});
 	}
